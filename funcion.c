b7069d71c80f2330055831ff6425a7df306aa2cd
montoyed <edgar.montoya.dse@uteq.edu.mx>
2018-09-07 12:36:43 -0500

    Added file funcion.c

---
+++ funcion.c
@@ -0,0 +1,11 @@
+// Archivo para el punto cuatro de la practica
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    printf("Hola mundo\n");
+    printf("Test_pratica_1.4.4.4");
+    return 0;
+}
